ec2ffc5f8c23ca5fe91b7d3d2cd4836230eb9bfe
ravinaNg <ravina18@navgurukul.org>
2019-01-11 23:08:27 +0530

    added comments

--- flask-demoy.py
+++ flask-demoy.py
@@ -3,16 +3,19 @@ from getInstance import getWordOfTheDay
 
 app = Flask(__name__)
 
+# "http://127.0.0.1:5000/login"
 @app.route('/login', methods=['GET'])
 def login():
     return "Login Hogya."
-"http://127.0.0.1:5000/get_word_of_the_day"
 
+# "http://127.0.0.1:5000/get_word_of_the_day"
 @app.route('/get_word_of_the_day', methods=['GET'])
 def getNewWord():
     data = {
         "newWord":getWordOfTheDay()
     }
     return jsonify(data)
-    
+
+# debug is True so we don't have to start the server again after 
+# making any change
 app.run(debug=True) # start the server.
