47bf6515bf77b2ab63a48c861db8c7be968091c9
shivam raj <shivamccgl@gmail.com>
2019-01-12 23:08:19 +0530

    Update flask-demoy.py

--- flask-demoy.py
+++ flask-demoy.py
@@ -10,7 +10,16 @@ def login():
 
 # "http://127.0.0.1:5000/get_word_of_the_day"
 @app.route('/get_word_of_the_day', methods=['GET'])
-def getNewWord():
+def getNewWord(level=0):
+    # level=0 don't use level
+    # level between 1 to 5
+    # return a word with larger length
+    # 1 - upto 4 characters
+    # 2 - upto 5 chars
+    # 3 - upto 7 chars
+    # 4 - upto 8 chars (higher probability)
+    # 5 - all
+
     data = {
         "newWord":getWordOfTheDay()
     }
