1ce047f7a77e219bb275588d09535da692b46152
Abhishek Gupta <abhishekgupta.iitd@gmail.com>
2019-01-29 13:21:33 +0530

    rona mat please

--- scripts/seed-database.js
+++ scripts/seed-database.js
@@ -1,16 +1,16 @@
-const async = require('async');
 const fs = require('fs');
-var _ = require('lodash')
-var knexfile = require('./knexfile.js');
+
+var knexfile = require('../knexfile.js');
 var knex = require('knex')(knexfile.development);
 
 const tableName = 'sentences';
 
-let rows = [];
+
 fs.readFile('hin.txt', function (err, data) {
   if (err) throw err;
-  // console.log(data.toString())
   let sentences = data.toString().split("\n")
+  let rows = [];
+
   for (let i = 0; i < sentences.length; i++) {
     const element = sentences[i];
     let e_h_sentences = element.split(/\t/g);
@@ -19,7 +19,6 @@ fs.readFile('hin.txt', function (err, data) {
     rows.push({ "sentence": e_sentence, "h_translation": h_sentence })
   }
 
-  // let delPromise =  knex(tableName).del();
   const promises = rows.map((row) => 
     new Promise((resolve, reject)=> {
         knex(tableName).count('id as a').where({"sentence":row["sentence"]}).then((count)=>{
